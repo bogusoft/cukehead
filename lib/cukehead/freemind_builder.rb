d881b91d38f2ee7e632f548933dbf830c95509d9
Bill Melvin <bill@bogusoft.com>
2010-05-02 20:53:10 -0400

    Worked on FreeMind Builder spec.

--- lib/cukehead/freemind_builder.rb
+++ lib/cukehead/freemind_builder.rb
@@ -4,6 +4,7 @@ class FreemindBuilder
 
   #DEFAULT_XML = "<map version=\"0.7.1\">\n<node TEXT=\"New mind map\">\n</node>\n</map>"
   DEFAULT_XML = "<map version='0.7.1'><node TEXT='New mind map'></node></map>"
+  COLOR_SYSTEM = '#a1a1a1'
 
   def initialize(mm_xml = nil)
     if mm_xml.nil? 
@@ -11,7 +12,46 @@ class FreemindBuilder
     else
       @mmdoc = REXML::Document.new(mm_xml)
     end
+    @features_path = nil    
   end
+  
+  def new_node_element(text, color = "", folded = false)
+    e = REXML::Element.new "node"
+    e.add_attribute 'TEXT', text
+    e.add_attribute 'COLOR', color if color.length > 0
+    e.add_attribute 'FOLDED', 'true' if folded
+    e
+  end
+
+  def add_cucumber_features_node
+    node = @mmdoc.root.elements[1]
+    e = new_node_element "Cucumber features:"
+    node.add_element e
+    #$stderr.puts "DEBUG: add_cucumber_features_node: " + e.to_s
+    return e
+  end
+
+  def cucumber_features_node
+    node = REXML::XPath.first(@mmdoc, '//node[attribute::TEXT="Cucumber features:"]')
+    if node == nil
+      add_cucumber_features_node
+    else
+      node
+    end
+  end
+
+  def features_path_text
+    '[path: ' + @features_path + ']'
+  end
+
+  def add_features_path(filename)
+    @features_path = File.dirname(File.expand_path(filename))
+    e = new_node_element features_path_text, COLOR_SYSTEM
+    e.add_attribute 'LINK', @features_path
+    @features_node.add_element e
+  end
+
+  
   
   def add_feature(feature, filename)
     # *!*
--- spec/freemind_builder_spec.rb
+++ spec/freemind_builder_spec.rb
@@ -3,12 +3,52 @@ require 'cukehead/feature_section'
 require 'cukehead/freemind_builder'
 
 describe "FreemindBuilder" do
+  before do
+    @builder = FreemindBuilder.new
+    #puts "BEFORE"
+  end
+  
+  it "should specify a XML node for Cucumber features" do
+    node = @builder.cucumber_features_node
+    node.should be_a REXML::Element    
+    node.to_s.should match /.*Cucumber features:.*/
+  end
+  
+  it "should use an existing node for Cucumber features if one exists" do
+    mm = "<map version='0.7.1'><node TEXT='mind map'><node TEXT='A child node'><node TEXT='Cucumber features:'></node></node></node></map>"
+    b = FreemindBuilder.new mm
+    node = b.cucumber_features_node
+    node.should be_a REXML::Element
+    xml = b.xml
+    doc = REXML::Document.new(xml)
+    node = REXML::XPath.first(@doc, '//node[attribute::TEXT="A child node"]')
+    child = node.elements.first
+    child.to_s.should match /.*Cucumber features:.*/
+  end
+  
+  it "should save the path to the features directory" do
+    #builder = FreemindBuilder.new
+    filename = '/tmp/some.feature'
+    @builder.add_features_path filename
+    @builder.features_path_text.should match /^\[path:.*some\.feature\]/
+  end
+  
+  it "should create new XML node element with text, color, and folded attributes" do
+    #builder = FreemindBuilder.new
+    text = "Test node"
+    color = "blue"
+    folded = true
+    node = @builder.new_node_element(text, color, folded)
+    node.should be_a REXML::Element
+    node.to_s.should match "<node FOLDED='true' TEXT='Test node' COLOR='blue'/>"
+  end
+  
   it "should add a feature and specify the name of the file it is from" do
     feature = FeatureSection.new
     feature.title = "Feature: Test feature"
     filename = "test.feature"
-    builder = FreemindBuilder.new
-    builder.add_feature feature, filename
-    builder.xml.should match /.*Test feature.*/ 
+    #builder = FreemindBuilder.new
+    @builder.add_feature feature, filename
+    @builder.xml.should match /.*Test feature.*/ 
   end
 end
