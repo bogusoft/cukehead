76150b69ef039c99215edda806a281470527adba
Bill Melvin <bill@bogusoft.com>
2010-05-05 08:42:56 -0400

    Added app_spec.rb and started specifying behaviors of cukehead app.

---
+++ spec/app_spec.rb
@@ -0,0 +1,24 @@
+require File.dirname(__FILE__) + '/spec_helper'
+#require 'cukehead/freemind_writer'
+
+describe "Cukehead application" do
+
+  it "reads a set of Cucumber features and create a FreeMind mind map"
+
+  it "looks for feature files in a 'features' sub-directory of the current directory by default"
+
+  it "accepts a features path to specify where to look for feature files"
+
+  it "creates a mind map file named cukehead.mm by default"
+
+  it "accepts a mind map file name to override the default"
+
+  it "does not overwrite an existing mind map file"
+
+  it "accepts an overwrite option that allows it to replace an exiting mind map file"
+
+  it "accepts an optional file name of an existing mind map"
+
+  it "adds feature nodes under an existing 'Cucumber features:' node"
+
+end
