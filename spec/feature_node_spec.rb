913fd19f1a54cde520ca7154252119420c3f1bdc
Bill Melvin <bill@bogusoft.com>
2010-05-15 08:17:36 -0400

    Added feature_node_spec.rb and freemind_reader_spec.rb.

---
+++ spec/feature_node_spec.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+module Cukehead
+
+  describe "FeatureNode" do
+
+    it "populates itself from a XML document node describing a Cucumber feature"
+
+    it "returns the file name for the feature as specified in the document"
+
+    it "creates a file name if none was specified based on the feature title"
+
+    it "returns itself in Cucumber feature file format as a single string of text"
+
+  end
+
+end
---
+++ spec/freemind_reader_spec.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+module Cukehead
+
+  describe "FreemindReader" do
+
+    #before do
+    #  @reader = FreemindReader.new
+    #end
+
+    it "takes a file name when initializing and loads it as an XML document"
+
+    it "can find a node containing Cucumber features in the XML document"
+
+    it "extracts features as text from the XML document as a hash {filename => text}"
+    
+  end
+
+end
