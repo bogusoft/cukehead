e4cd177d65b4651aa8bfc8cdf161d794ee265c35
Bill Melvin <bill@bogusoft.com>
2010-07-12 20:42:08 -0400

    Worked on spec for bug in FeatureReader where missing feature section is not handled.

--- features/step_definitions/cukehead_steps.rb
+++ features/step_definitions/cukehead_steps.rb
@@ -1,14 +1,30 @@
+$:.push File.join(File.dirname(__FILE__), '..', '..', 'lib')
+require 'cukehead/feature_reader'
+
+
 Given /^a feature file with:$/ do |text|
   @text = text
 end
 
-When /^the FeatureReader parses the text from the file$/ do
-  @reader = FeatureReader.new(text)
+
+When /^I read the feature into a mind map$/ do
+  @reader = Cukehead::FeatureReader.new
+  require 'rubygems'; require 'ruby-debug'; debugger
+  @reader.extract_features 'testdata.feature', @text
 end
 
-Then /^there should be a feature section titled "Do something'$/ do
-  features = @reader.getFeatures
-  found = false
-  features.each {|feature| found = true if feature.title == "Do something"}
-  found.should_be true
+Then /^the mind map XML should contain a node with the TEXT attribute "([^\"]*)"$/ do |arg1|
+  xml = @reader.freemind_xml
+  xml.should match /#{arg1}/
 end
+
+#When /^the FeatureReader parses the text from the file$/ do
+#  @reader = FeatureReader.new(text)
+#end
+
+#Then /^there should be a feature section titled "Do something'$/ do
+#  features = @reader.getFeatures
+#  found = false
+#  features.each {|feature| found = true if feature.title == "Do something"}
+#  found.should_be true
+#end
--- mm/CukeHead.mm
+++ mm/CukeHead.mm
@@ -10,12 +10,20 @@
 <node TEXT="Feature: CukeHead Map">
 <node TEXT="As a person learning or using Cucumber"/>
 <node TEXT="I want to view Cucumber features as a FreeMind mind map"/>
+<node TEXT="Scenario: Missing Feature section">
+<node TEXT="Given a feature file with:"/>
+<node TEXT="&quot;&quot;&quot;&#xa;Scenario: foo&#xa;&quot;&quot;&quot;"/>
+<node TEXT="When I read the feature into a mind map"/>
+<node TEXT="Then I should see an exception matching &quot;no feature&quot;"/>
+</node>
+</node>
+</node>
+<node TEXT="Pending features">
 <node TEXT="Scenario: Scenario Outline">
 <node TEXT="Given a feature file with:"/>
 <node TEXT="&quot;&quot;&quot;&#xa;Scenario Outline: eating&#xa;  Given there are &lt;start&gt; cucumbers&#xa;  When I eat &lt;eat&gt; cucumbers&#xa;  Then I should have &lt;left&gt; cucumbers&#xa;&#xa;  Examples:&#xa;    | start | eat | left |&#xa;    |  12   |  5  |  7   |&#xa;    |  20   |  5  |  15  |&#xa;&quot;&quot;&quot;"/>
 <node TEXT="When I read the feature into a mind map"/>
 <node TEXT="Then the mind map XML should contain a node with the TEXT attribute &quot;Scenario Outline: eating&quot;"/>
-</node>
 </node>
 </node>
 </node>
